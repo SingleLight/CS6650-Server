2648f7faf120fc57d5787dbd7745c9b07d6a7bb6
shanmingyuan <mshan@u.rochester.edu>
2022-02-04 13:43:51 -0800

    implemented servlet url validation

--- src/main/java/SkiServlet.java
+++ src/main/java/SkiServlet.java
@@ -11,6 +11,26 @@ public class SkiServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
+    response.setContentType("application/json");
+    String urlPath = request.getPathInfo();
+    if (urlPath == null || urlPath.isEmpty()) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      response.getWriter().write("missing parameters");
+      return;
+    }
+    String[] urlParts = urlPath.split("/");
+    if (!isGetUrlValid(urlParts)) {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      response.getWriter().write("Bad Url");
+    } else {
+      response.setStatus(HttpServletResponse.SC_OK);
+      response.getWriter().write("It works!");
+    }
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
 
     response.setContentType("application/json");
     String urlPath = request.getPathInfo();
@@ -23,44 +43,52 @@ public class SkiServlet extends HttpServlet {
 
     String[] urlParts = urlPath.split("/");
 
-    if (!isGetUrlValid(urlParts)) {
-      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    if (!isPostUrlValid(urlParts)) {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      response.getWriter().write("Bad Url");
     } else {
       response.setStatus(HttpServletResponse.SC_OK);
-
       response.getWriter().write("It works!");
     }
   }
 
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-
+  private boolean isGetUrlValid(String[] urlParts) {
+    int length = urlParts.length - 1;
+    switch (length) {
+      case 1:
+        return lengthOneUrlValidation(urlParts);
+      case 3:
+        return lengthThreeUrlValidation(urlParts);
+      case 7:
+        return lengthSevenUrlValidation(urlParts);
+      case 8:
+        return lengthEightUrlValidation(urlParts);
+    }
+    return false;
   }
 
-  private boolean isGetUrlValid(String[] urlParts) {
-    int length = urlParts.length;
-    if (length == 1) {
-      return lengthOneUrlValidation(urlParts);
-    } else if (length == 7) {
-      return lengthSevenUrlValidation(urlParts);
-    } else if (length == 3) {
-      return lengthThreeUrlValidation(urlParts);
+  private boolean isPostUrlValid(String[] urlParts) {
+    int length = urlParts.length - 1;
+    switch (length) {
+      case 3:
+        return postLengthThreeUrlValidation(urlParts);
+      case 8:
+        return postLengthEightUrlValidation(urlParts);
     }
     return false;
   }
 
   private boolean lengthOneUrlValidation(String[] urlParts) {
-    return urlParts[0].equals("resorts") || urlParts[0].equals("statistics");
+    return urlParts[1].equals("resorts") || urlParts[1].equals("statistics");
   }
 
   private boolean lengthSevenUrlValidation(String[] urlParts) {
-    if (urlParts[0].equals("resorts") && urlParts[2].equals("seasons") && urlParts[4].equals(
-        "day") && urlParts[6].equals("skiers")) {
+    if (urlParts[1].equals("resorts") && urlParts[3].equals("seasons") && urlParts[5].equals("day")
+        && urlParts[7].equals("skiers")) {
       try {
-        Integer.parseInt(urlParts[1]);
-        Integer.parseInt(urlParts[3]);
-        Integer.parseInt(urlParts[5]);
+        Integer.parseInt(urlParts[2]);
+        Integer.parseInt(urlParts[4]);
+        Integer.parseInt(urlParts[6]);
       } catch (NumberFormatException e) {
         return false;
       }
@@ -70,16 +98,16 @@ public class SkiServlet extends HttpServlet {
   }
 
   private boolean lengthThreeUrlValidation(String[] urlParts) {
-    if (urlParts[0].equals("resorts") && urlParts[2].equals("seasons")) {
+    if (urlParts[1].equals("resorts") && urlParts[3].equals("seasons")) {
       try {
-        Integer.parseInt(urlParts[1]);
+        Integer.parseInt(urlParts[2]);
       } catch (NumberFormatException e) {
         return false;
       }
       return true;
-    } else if (urlParts[0].equals("skiers") && urlParts[2].equals("vertical")) {
+    } else if (urlParts[1].equals("skiers") && urlParts[3].equals("vertical")) {
       try {
-        Integer.parseInt(urlParts[1]);
+        Integer.parseInt(urlParts[2]);
       } catch (NumberFormatException e) {
         return false;
       }
@@ -89,18 +117,34 @@ public class SkiServlet extends HttpServlet {
   }
 
   private boolean lengthEightUrlValidation(String[] urlParts) {
-    if (urlParts[0].equals("skiers") && urlParts[2].equals("seasons") && urlParts[4].equals(
-        "day") && urlParts[6].equals("skiers")) {
+    if (urlParts[1].equals("skiers") && urlParts[3].equals("seasons") && urlParts[5].equals("day")
+        && urlParts[7].equals("skiers")) {
       try {
-        Integer.parseInt(urlParts[1]);
-        Integer.parseInt(urlParts[3]);
-        Integer.parseInt(urlParts[5]);
-        Integer.parseInt(urlParts[7]);
+        Integer.parseInt(urlParts[2]);
+        Integer.parseInt(urlParts[4]);
+        Integer.parseInt(urlParts[6]);
+        Integer.parseInt(urlParts[8]);
       } catch (NumberFormatException e) {
         return false;
       }
       return true;
     }
     return false;
+  }
+
+  private boolean postLengthThreeUrlValidation(String[] urlParts) {
+    if (urlParts[1].equals("resorts") && urlParts[3].equals("seasons")) {
+      try {
+        Integer.parseInt(urlParts[2]);
+      } catch (NumberFormatException e) {
+        return false;
+      }
+      return true;
+    }
+    return false;
+  }
+
+  private boolean postLengthEightUrlValidation(String[] urlParts) {
+    return lengthEightUrlValidation(urlParts);
   }
 }
